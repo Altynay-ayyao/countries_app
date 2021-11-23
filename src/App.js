5e39e1cbc3ae56c62b4131c8e61da2585ec5e4fa
Sabitzhan Altynay <s2100149@edu.bc.fi>
2021-11-23 10:23:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  };
+  componentDidMount() {
+    axios
+      .get(
+        "https://restcountries.com/v2/all?fields=name,capital,flags,languages,population"
+      )
+      .then((res) => {
+        this.setState({ data: res.data });
+        console.log(this.state.data);
+      });
+  }
+  render() {
+    return (
+      <>
+        <div className="flag-wrap">
+          {this.state.data.map((country) => (
+            <div className="flag-single" key={country.name}>
+              <h2>{country.name}</h2>
+
+              <p>
+                capital: <span>{country.capital}</span>
+              </p>
+
+              <p>
+                language(s):{" "}
+                {country.languages.map((lang) => (
+                  <span>{lang.name} </span>
+                ))}
+              </p>
+
+              <p>
+                population:{" "}
+                <span>{country.population.toLocaleString("fr")}</span>
+              </p>
+
+              <img src={country.flags.svg} alt="#" />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
